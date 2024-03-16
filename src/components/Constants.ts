a0732c780be2c9a3312af2f149c16d1be4bbb4a0
Ray Smets <rayjsmets@gmail.com>
2024-03-15 18:43:18 -0700

    jup and dexscreener

--- src/components/Constants.ts
+++ src/components/Constants.ts
@@ -8,12 +8,13 @@ export const addresses = {
 };
 
 export const urls = {
-  birdeye: `https://dexscreener.com/solana/${addresses.token}`,
+  birdeye: `https://birdeye.so/token/${addresses.token}?chain=solana`,
+  dexscreener: `https://dexscreener.com/solana/${addresses.token}`,
   raydiumlp: `https://raydium.io/liquidity/add/?coin0=${addresses.token}&coin1=sol&fixed=coin0&ammId=AVs9TA4nWDzfPJE9gGVNJMVhcQy3V9PGazuz33BfG2RA`,
   raydiumbuy: `https://raydium.io/swap/?inputCurrency=sol&outputCurrency=${addresses.token}&outputSymbol=AF&fixed=in`,
   githuborg: "https://github.com/orgs/JT-Labs-LLC",
   telegram: "https://t.me/+YLRmwGGZMBYyNzEx",
   twitter: "https://twitter.com/AstroFrog_AF",
-  jupiter: `https://jup.ag/swap/SOL-AF_${addresses.token}`,
+  jupiter: `https://t0.jup.ag/swap/AF_${addresses.token}-SOL`,
   explorer: `https://solana.fm/address/${addresses.token}`,
 };
